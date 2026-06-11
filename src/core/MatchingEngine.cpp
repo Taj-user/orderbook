0f4ff481902b4500e91f5d4dd19767b9cb45054b
Taj-user <robtajzzz@icloud.com>
2026-06-11 17:26:26 -0500

    Still working on submit method. TODO: matching loop

--- src/core/MatchingEngine.cpp
+++ src/core/MatchingEngine.cpp
@@ -2,10 +2,31 @@
 
 void MatchingEngine::submit(Order* order) {
     // Validation
-    if(order == nullptr || (order->order_type_ == OrderType::LIMIT && order->price_ == std::nullopt) || (order->order_type_ == OrderType::MARKET && order->price_ != std::nullopt) || order->quantity_original_ <= 0) {
+    if(order == nullptr) return;
+    if((order->order_type_ == OrderType::LIMIT && order->price_ == std::nullopt) || (order->order_type_ == OrderType::MARKET && order->price_ != std::nullopt) || order->quantity_original_ <= 0) {
         order->status_ = OrderStatus::REJECTED;
         return;
     }
 
-    // TODO: FOK precheck
+    auto process = [&](auto& opposing_side) {
+        // FOK check
+        if(order->time_in_force_ == TimeInForce::FOK) {
+            u64 sum = 0;
+            for(const auto& [price, level] : opposing_side) {
+                if(order->side_ == Side::BUY && order->price_ && price > *order->price_) break;
+                if(order->side_ == Side::SELL && order->price_ && price < *order->price_) break;
+                sum += level.totalQuantity();
+            }
+            if(sum < order->quantity_remaining_) {
+                order->status_ = OrderStatus::REJECTED;
+                return;
+            }
+        }
+        // Matching loop
+        for(auto& [price, level] : opposing_side) {
+            
+        }
+    };
+    if(order->side_ == Side::BUY) process(book_.asks());
+    else process(book_.bids());
 }
