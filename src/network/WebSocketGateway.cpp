e60af45d8f8a398098c95fe16f36cf6476bfe983
Taj-user <robtajzzz@icloud.com>
2026-08-06 20:41:45 -0500

    Working on websocket accept and read

--- src/network/WebSocketGateway.cpp
+++ src/network/WebSocketGateway.cpp
@@ -11,3 +11,28 @@ void WebSocketGateway::start() {
         accept();
         m_thread = std::thread([this]() { m_ioc.run(); });
 }
+
+void WebSocketGateway::stop() {
+        m_ioc.stop();
+        m_thread.join();
+}
+
+void WebSocketGateway::accept() {
+        m_acceptor.async_accept([this](beast::error_code ec, tcp::socket socket) {
+                if(!ec) {
+                        // create session, do handshake, start reading
+                        auto session = std::make_shared<Session>(std::move(socket));
+                        session->m_ws.accept(ec);
+                        m_sessions.push_back(session);
+
+                        session->m_ws.async_read(session->m_buffer, [this, session](beast::error_code ec, size_t bytes) {
+                                if(!ec) {
+                                        std::string message = beast::buffers_to_string(session->m_buffer.data());
+                                        session->m_buffer.consume(session->m_buffer.size());
+                                        on_message(message);
+                                }
+                        });
+                }
+                accept();
+        });
+}
